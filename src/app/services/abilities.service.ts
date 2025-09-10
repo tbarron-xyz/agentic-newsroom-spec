35b1aee98424ee3da6def25ac9a5014107672a95
Thomas Barron <t@tbarron.xyz>
2025-09-10 12:45:42 -0600

    feat: Admin users now have all abilities granted automatically

--- src/app/services/abilities.service.ts
+++ src/app/services/abilities.service.ts
@@ -2,18 +2,21 @@ import { User } from '../models/types';
 
 export class AbilitiesService {
   userIsReader(user: User): boolean {
+    if (user.role === 'admin') return true;
     return user.hasReader;
   }
 
   userIsReporter(user: User): boolean {
+    if (user.role === 'admin') return true;
     return user.hasReporter;
   }
 
   userIsEditor(user: User): boolean {
+    if (user.role === 'admin') return true;
     return user.hasEditor;
   }
 
   userIsAdmin(user: User): boolean {
-    return user.hasEditor;
+    return user.role === 'admin';
   }
 }
